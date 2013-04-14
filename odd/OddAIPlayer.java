cbed94cf5ac5f86f854410b1815fa7777af7f13a
Sami Jaber <jabersami@gmail.com>
2013-04-14 18:27:08 -0400

    AI Project

---
+++ odd/OddAIPlayer.java
@@ -0,0 +1,22 @@
+package odd;
+
+import boardgame.Board;
+import boardgame.Move;
+import boardgame.Player;
+
+public class OddAIPlayer extends Player {
+
+	public OddAIPlayer(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Move chooseMove(Board board) {
+		
+		
+		
+		return null;
+	}
+
+}
